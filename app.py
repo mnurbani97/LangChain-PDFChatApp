9de1b9ae49d96da305e128e16be94a663d1b155a
Mardhya Malik Nurbani <mnurbani97@gmail.com>
2024-07-17 16:19:19 +0700

    update

--- app.py
+++ app.py
@@ -14,17 +14,36 @@ import os
 
 # Sidebar content
 with st.sidebar:
-    st.markdown('## About')
-    add_vertical_space(5)
-    st.write('eCentrix Solutions')
+    image = Image.open('eCentrixlogo.png')
+    st.image(image, caption = 'eCentrix Solutions')
+    # st.markdown('## About')
+    # add_vertical_space(5)
+    # st.write('eCentrix Solutions')
 
 load_dotenv()
 
 def main():
+    image = Image.open('ecentrix.png')
+    st.image(image, caption = '')
     st.header('Chat With Your PDF')
 
+    with st.form('Assist ur PDF with ChatGPT'):
     # Upload PDF
-    pdf = st.file_uploader("Upload Your PDF", type='pdf')
+        pdf = st.file_uploader("Upload Your PDF", type='pdf')
+
+        # submitted = st.form_submit_button()
+    
+
+    # if 'messages' not in st.session_state:
+    #     st.session_state.messages = []
+    # with st.form(key='chat_form'):
+    #     user_input = st.text_input("Masukkan pesan:")
+    #     submit_button = st.form_submit_button(label='Kirim')
+
+    # # Jika tombol diklik, simpan pesan ke sesi
+    # if submit_button and user_input:
+    #     st.session_state.messages.append({"user": user_input, "bot": "Balasan dari chatbot."})
+        
     if pdf is not None:
         st.write(f"Uploaded PDF: {pdf.name}")
 
@@ -69,7 +88,8 @@ def main():
                 response = chain.run(input_document=docs, question=query)
                 print(cb)
             st.write(response)
-        
+
+
 
 if __name__ == '__main__':
     main()
